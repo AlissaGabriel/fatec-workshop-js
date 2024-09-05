d17defafae7221c5964f20d03abec67607b92823
AlissaGabriel <alissagabriel14@gmail.com>
2024-09-05 15:58:01 -0300

    Resolução do desafio proposto

---
+++ resolucao.js
@@ -0,0 +1,14 @@
+//Alissa Gabriel
+//2 Semestre - Desenvolvimento De Software Multiplataforma
+
+const y = 5;
+let x = 5;
+x += 10;
+
+
+function somar () {
+   const r = x + y;
+   return r;
+}
+
+console.log(somar());
